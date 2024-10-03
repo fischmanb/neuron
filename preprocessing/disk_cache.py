5168308967aa018e92a3bc6097a5d70fcd5081fd
Mark O'Brien <mobrien@no_reply.com>
2024-10-03 11:04:43 -0400

    Add debug

--- preprocessing/disk_cache.py
+++ preprocessing/disk_cache.py
@@ -99,13 +99,12 @@ def main():
 
     segments_list = list(segments_list)
     print(f"Loaded segments list {len(list(segments_list))} files")
-
-    for item in segments_list:
+    for idx, item in enumerate(segments_list):
         alt_filename = f"{item.split('/')[1]}.pt"
         if os.path.exists(f"{savepath}/{alt_filename}"):
-            print("skipping")
+            print(f"skipping {idx}: {alt_filename}")
         else:
-            gstor.download_blob_to_disk_chris(mfcc_bucket, item, local_file_path=savepath, alt_filename=alt_filename, verbose=False)
+            gstor.download_blob_to_disk_chris(mfcc_bucket, item, local_file_path=savepath, alt_filename=alt_filename, verbose=True)
 
 
 if __name__ == '__main__':
